9ab11744535c5c225663ea6bae6b1db4a957c522
PaintNinja <paintninjamail-projects@yahoo.co.uk>
2014-09-22 14:51:12 +0100

    Put the sprinkles on top

--- homepage.js
+++ homepage.js
@@ -1,5 +1,40 @@
 var iFrameID = "contentframe"
-var newPage = "http://www.msn.com";
-function ChangePage() {
+var newPageDash = "http://minetrends.com/dashboard";
+var newPageStatus = "http://minetrends.com/statistics/serverhealth";
+var newPagePlayers = "http://minetrends.com/statistics/players";
+function ChangePageDash() {
 var iFrameObject = document.getElementById(iFrameID);
-iFrameObject.src = newPage;
+iFrameObject.src = newPageDash;
+}
+function ChangePageStatus() {
+var iFrameObject = document.getElementById(iFrameID);
+iFrameObject.src = newPageStatus;
+}
+function ChangePagePlayers() {
+var iFrameObject = document.getElementById(iFrameID);
+iFrameObject.src = newPageStatus;
+}
+<!--
+function resize_iframe()
+{
+
+	var height=window.innerWidth;//Firefox
+	if (document.body.clientHeight)
+	{
+		height=document.body.clientHeight;//IE
+	}
+	//resize the iframe according to the size of the
+	//window (all these should be on the same line)
+	document.getElementById("contentframe").style.height=parseInt(height-
+	document.getElementById("contentframe").offsetTop-8)+"px";
+}
+
+// this will resize the iframe every
+// time you change the size of the window.
+window.onresize=resize_iframe; 
+
+//Instead of using this you can use: 
+//	<BODY onresize="resize_iframe()">
+
+
+//-->
